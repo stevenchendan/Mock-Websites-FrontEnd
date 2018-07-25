3db3ba0cae7ad7f9425e705a139c57b0a62dc8cf
stevenchendan <qwe16165850@gmail.com>
2018-07-25 22:51:38 +1000

    add icons to production page

--- ckeditor/mock-ckeditor-vue/src/components/productionPage.vue
+++ ckeditor/mock-ckeditor-vue/src/components/productionPage.vue
@@ -15,11 +15,12 @@
         Learn more</a>
     </div>
     <div class="production-card production-cloud-service">
-      <h3>Editor Cloud Service</h3>
-      <p>Lorem ipsum dolor sit amet, mei quas sententiae ut. 
-        Vel exerci possim deleniti ad. Ex nam</p>
       <a class="" href="">
-        Learn more</a>
+        <h3>Editor Cloud Service</h3>
+        <p>Lorem ipsum dolor sit amet, mei quas sententiae ut. 
+          Vel exerci possim deleniti ad. Ex nam</p>
+        <span class="btn">Learn more</span>
+      </a>
     </div>
   </section>
 </template>
@@ -33,12 +34,48 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .production-section {
-  position: relative;
-  margin-top: 100vh;
-  
+  display: inline-flex;
+  margin-top: 110vh;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  height: 30vh;
+  max-width: none;
+}
+.btn {
+  color: #fff;
+  border-color: #2e9fff;
+  background-color: #2e9fff;
 }
 
+.production-card {
+  position: relative;
+  text-align: center;
+  overflow: hidden;
+  border-radius: 4px;
+  transition: all .2s ease-in-out;
+}
+.production-card::after {
+    content: '';
+    position: absolute;
+    top: 2.5rem;
+    left: 50%;
+    width: 104px;
+    height: 97px;
+    margin-left: 8px;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center top;
+    transform: translate3d(-50%,0,0);
+    z-index: 3;
+}
 .production-editor-5::after {
     background-image: url(../assets/images/product/icon-ckeditor-5.svg);
+}
+.production-editor-4::after {
+    background-image: url(../assets/images/product/icon-ckeditor-4.svg);
+}
+.production-cloud-service::after {
+    background-image: url(../assets/images/product/icon-cloud.svg);
 }
 </style>
